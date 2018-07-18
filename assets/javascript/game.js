918432f2c68e72441ab835650f4e7db95019da49
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-18 19:58:08 -0400

    starting over from line 6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,10 @@
-var computerChoices = ["a", "b", "c"];
+var secretLetter = "a, b, c";
 
 var wins = 0;
 var losses = 0;
 varguessLeft = 9;
-// var guessTyped = userGuess;
+var guessTyped 
 
-// fuction starts when user presses any key
-document.onkeyup = function (event) {
-
-    // determines key pressed
-    var userGuess = event.key;
-
-    var randomGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c")) {
-        if (userGuess === randomGuess) {
-            wins++;
-        } else {
-            losses++;
-        }
-        var html =
-            "<p>Guess what letter i'm thinking of: " + userGuess + "</p>" +
-            "<p>wins: " + wins + "</p>" +
-            "<p>losses: " + losses + "</p>"
-
-
-
-        document.querySelector("#game").innerHTML = html;
-
-    }
-
-
-}
 
 
 
