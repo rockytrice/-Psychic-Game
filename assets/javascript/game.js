b2ded223730c29a0012e55276670aee027f4488a
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-19 00:04:33 -0400

    made on key function and if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,28 +6,34 @@ var wins = 0;
 var losses = 0;
 var guessLeft = 9;
 // randomly selects letter from array
-var physicLetter = secretLetter[math.floor.(math.random() * secretLetter.length)];
+var physicLetter = secretLetter[Math.floor(Math.random() * secretLetter.length)];
 
 // when user presses key, code will run
 document.onkeyup = function (event) {
     var userGuess = event.key;
-    if (userGuess===physicLetter){
-        wins ++;
+    if (userGuess === physicLetter) {
+        wins++;
     } else {
-        guessLeft --;
+        guessLeft--;
     }
-}
+    if (userGuess === 0) {
+        losses++;
+    }
+    var html =
+        "<h1>The Pyschic Game:" + "</h2>" +
+        "<p>wins: " + wins + "</p>" +
+        "<p>losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessLeft + "</p>" +
+        "<p> Your Guesses so far " + userGuess + "</p>"
+
+    // Set the inner HTML contents of the #game div to our html string
+    document.querySelector("#game").innerHTML = html;
+
+};
 
 
 
-        // var html =
-        //     "<p>Guess what letter i'm thinking of: " + userText + "</p>" +
-        //     "<p>wins: " + wins + "</p>" +
-        //     "<p>losses: " + losses + "</p>" +
-        //     "<p>
 
-        // // Set the inner HTML contents of the #game div to our html string
-        // document.querySelector("#game").innerHTML = html;
 
 
 
